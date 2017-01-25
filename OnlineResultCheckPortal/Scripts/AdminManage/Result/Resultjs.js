e7ad83cfe79bd00ff66e7f15ae228b3bae183598
salamtura <salamtura@gmail.com>
2017-01-25 01:35:32 +0100

    updates to contact page and fresults page datatable fix

--- OnlineResultCheckPortal/Scripts/AdminManage/Result/Resultjs.js
+++ OnlineResultCheckPortal/Scripts/AdminManage/Result/Resultjs.js
@@ -89,16 +89,22 @@ function SearchResult(Controller) {
                     $('#lblMessage').html("Please purchase new token.");
                     setTimeout(function () { $("#lblMessage").hide(); }, 10000);
                 }
+                else if (d == "6") {
+                    $("#lblMessage").show();
+                    $('#lblMessage').html("Token use has been exceeded");
+                    setTimeout(function () { $("#lblMessage").hide(); }, 10000);
+                }
                 else {
 
-                    var oTable = $('#UserDetailsTable').dataTable();
+                    var oTable = $('#UserDetailsTable').dataTable({ paging: false, "sDom": "" });
                     oTable.fnClearTable();
                     $('#UserDetailsTables').show();
+                    console.log(d);
                     //Append for loop row to html table
                     for (var i = 0; i < d.length; i++) {
                         $('#fileResult').val(d[i].RegistrationNumber);
                         $('#UserDetailsTable').dataTable().fnAddData([
-                        d[i].RowNumber, d[i].SubjectName, d[i].Grade, d[i].Remarks //"<a  href='#'  onclick='DeleteUserProfile(" + d[i].ID + ")'class='glyphicon glyphicon-trash' alt='Mountain View' style='width:15px;height:18px;margin-left:2px;color: red;'></a>&nbsp;|&nbsp;<a href='#'  data-toggle='modal' data-target='#myStudentProfile' onclick='DisplayStudentProfile(" + d[i].ID + ")'/><img src='/Images/details-icon-png-cc-by-3-0--it-1.PNG' alt='Mountain View' style='width:17px;height:15px;margin-left:2px;'></a>"
+                         d[i].SubjectName, d[i].Grade, d[i].Remarks //"<a  href='#'  onclick='DeleteUserProfile(" + d[i].ID + ")'class='glyphicon glyphicon-trash' alt='Mountain View' style='width:15px;height:18px;margin-left:2px;color: red;'></a>&nbsp;|&nbsp;<a href='#'  data-toggle='modal' data-target='#myStudentProfile' onclick='DisplayStudentProfile(" + d[i].ID + ")'/><img src='/Images/details-icon-png-cc-by-3-0--it-1.PNG' alt='Mountain View' style='width:17px;height:15px;margin-left:2px;'></a>"
 
                         ]);
 
